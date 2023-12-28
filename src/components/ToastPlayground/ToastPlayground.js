79ca3a15a6d8cbb57805f2b3d16e3e88226f711f
James Acres <jamesacres@gmail.com>
2023-12-28 21:17:58 +0000

    show dynamic toast

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -3,18 +3,32 @@ import React from 'react';
 import Button from '../Button';
 
 import styles from './ToastPlayground.module.css';
+import Toast from '../Toast';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
   const [message, setMessage] = React.useState();
   const [selectedVariant, setSelectedVariant] = React.useState();
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  const onDismiss = () => {
+    setIsVisible(false);
+  };
   return (
     <div className={styles.wrapper}>
       <header>
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      {isVisible && (
+        <Toast
+          message={message}
+          variant={selectedVariant}
+          onDismiss={onDismiss}
+        />
+      )}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -62,7 +76,7 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button onClick={() => setIsVisible(!isVisible)}>Pop Toast!</Button>
           </div>
         </div>
       </div>
